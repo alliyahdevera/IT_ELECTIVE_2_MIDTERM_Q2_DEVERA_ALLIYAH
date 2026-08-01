0b04729141fe4c2956286f01d8303543e47445eb
alliyahdevera <alliyahalceldevera@gmail.com>
2026-08-01 09:08:37 +0800

    Spotify Color Design

--- ConsoleApp1/WebApplication1/Data/MockDatabase.cs
+++ ConsoleApp1/WebApplication1/Data/MockDatabase.cs
@@ -9,12 +9,12 @@ namespace WebApplication1.Data
         {
             new Playlist
             {
-                Title = "Chill OPM Acoustic Jam",
+                Title = "Favs Spider-Man",
                 CreatorName = "System",
                 Videos = new List<VideoItem>
                 {
-                    new VideoItem { YouTubeUrl = "(https://youtu.be/)...", TrackNote = "Classic Eraserheads"},
-                    new VideoItem { YouTubeUrl = "(https://youtu.be/)...", TrackNote = "Great vocal harmony" }
+                    new VideoItem { YouTubeUrl = "(https://www.youtube.com/watch?v=ApXoWvfEYVU)", TrackNote = "Post Malone, Swae Lee - Sunflower"},
+                    new VideoItem { YouTubeUrl = "(https://youtu.be/Y88LVU7MAe4?si=iOwPAGmgYYtHCGuP)", TrackNote = "Blackway & Black Caviar - What's Up Danger" }
                 }
             }
         };
